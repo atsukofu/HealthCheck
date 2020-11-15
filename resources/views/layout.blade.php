8d99b840dc1c0b825abc0b216da99f5364a5994a
atsukofu <atsukofu0527@gmail.com>
2020-11-15 10:26:39 +0900

    ナビゲーションバーにレスポンシブデザインを適用

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,10 +10,31 @@
 <body>
   <nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
     <a class='navbar-brand' href="{{route('condition.new')}}">HealthCheck</a>
-    <a class='navbar-brand' href="{{route('condition.menu')}}" style="margin-left:30px;">menu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-start" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class='nav-link active' href="{{route('condition.menu')}}">menu</a>
+        </li>
+        <li class="nav-item">
+          <a class='nav-link' href="#">logout</a>
+        </li>
+      </ul>
+    </div>
   </nav>
   <div class='container'>
     @yield('content')
   </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 </body>
 </html>
