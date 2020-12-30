aaec370b49b493f2d9378b341352fcd46cfde8ec
atsukofu <atsukofu0527@gmail.com>
2020-12-30 16:02:40 +0900

    テーブルの削除

--- resources/views/condition/new.blade.php
+++ resources/views/condition/new.blade.php
@@ -8,7 +8,7 @@
 @endif
 
 <h2 style="margin-bottom: 50px";>データを入力してください</h2>
-<table class="table table-striped table-hover">
+<!-- <table class="table table-striped table-hover">
 {{ Form::open(['route' => 'condition.store', 'name' => 'form']) }}
   <tr>
     <div class="form-group">
@@ -25,7 +25,7 @@
     </div>
   </tr>
   <tr>
-    <!-- <div class="form-group">
+    <div class="form-group">
       <th>{{ Form::label('nail', '爪の長さ:') }}</th>
       <td><lavel>OK{{ Form::radio('nail', 1, true) }}&emsp;</lavel>
       <lavel>NG{{ Form::radio('nail', 2) }}</lavel></td>
@@ -51,10 +51,10 @@
       <td><lavel>OK{{ Form::radio('other', 1, true) }}&emsp;</lavel>
       <lavel>NG{{ Form::radio('other', 2) }}</lavel></td>
     </div>
-  </tr> -->
+  </tr>
   <tr><th></th><td>{{ Form::submit('登録する',['class' => "btn btn-primary", 'id' => 'submit-btn', 'data-toggle' => 'modal','data-target' => '#testModal'])}}</td>
   {{ Form::close() }}
-</table>
+</table> -->
 
 <!-- モーダル -->
 <div class="modal fade" id="confirmModal" tabindex="1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
